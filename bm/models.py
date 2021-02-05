94fe2cfb96f133f3d514eb8c67992ef258ac704c
Ahmed Lotfy <aelr_211044@yahoo.com>
2021-02-05 23:38:21 +0200

    'add-book'

--- bm/models.py
+++ bm/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+          name=models.CharField(max_length=50)
+          
+                    
+          def __str__(self):
+                    return self.name
+          
+class Book(models.Model):
+          status_book=[
+                    ("availble","availble"),
+                    ("rental","rental"),
+                    ("sold",'sold'),
+          ]
+          
+          title = models.CharField(max_length=250)
+          author = models.CharField(max_length=250)
+          photo_book=models.ImageField(upload_to='photos',null=True,blank=True)
+          photo_author=models.ImageField(upload_to='photos',null=True,blank=True)
+          pages=models.IntegerField(null=True,blank=True)
+          price=models.IntegerField(null=True,blank=True)
+          retal_price_day=models.DecimalField(max_digits=5,decimal_places=3,null=True,blank=True)
+          retal_proid=models.IntegerField(null=True,blank=True)
+          active=models.BooleanField(default=True)
+          status=models.CharField(max_length=50,choices=status_book,null=True,blank=True)
+          category=models.ForeignKey(Category,on_delete=models.PROTECT)
+          
+          def __str__(self):
+                    return self.title
--- bm/views.py
+++ bm/views.py
@@ -1,4 +1,45 @@
 from django.shortcuts import render
+from .models import *
+from .forms import BookForm ,CategoryForm
 
 def index(request):
-          return render(request,'pages/index.html')
+          if request.method == "POST":
+                    data=BookForm(request.POST,request.FILES)
+                    if data.is_valid():   
+                              data.save()
+                    add_category=CategoryForm(request.POST)
+                    if add_category.is_valid():
+                              add_category.save()
+                              
+          books    =Book.objects.all()
+          categorys=Category.objects.all()
+          form     = BookForm()
+          formc    = CategoryForm()
+          context={
+                    'books':books,
+                    'categorys':categorys,
+                    'form'     :form,
+                    'formc'    :formc,
+                    
+          }
+          return render(request,'pages/index.html',context)
+
+
+def books(request):
+          if request.method == "POST":
+                    
+                    add_category=CategoryForm(request.POST)
+                    if add_category.is_valid():
+                              add_category.save()
+                              
+          books    =Book.objects.all()
+          categorys=Category.objects.all()
+          formc    = CategoryForm()
+          context={
+                    'books'    :books,
+                    'categorys':categorys,
+                    'formc'    :formc,
+                    
+                    
+          }
+          return render(request,'pages/books.html',context)
